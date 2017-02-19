18dec033f3af240c346875fb4535cc6de1608fc0
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-02-19 13:58:48 +0100

    16th commit

--- testhook.js
+++ testhook.js
@@ -6,24 +6,7 @@ var bodyParser = require('body-parser');
 var join = require('path').join;
 var http = require('http');
 var path = require('path');
-var execFile = require('child_process').execFile;
-/*
-var connection = mysql.createConnection({
-  host     : '109.135.3.222',
-  //host     : '192.168.69.190',
-  user     : 'mobileapp',
-  password : 'exji-0lj+cNQiX$nsG',
-  database : 'pants'
-});
-
-var connection2 = mysql.createConnection({
-  host     : '109.135.3.222',
-  //host     : '192.168.69.190',
-  user     : 'mobileapp',
-  password : 'exji-0lj+cNQiX$nsG',
-  database : 'users'
-});
-*/
+var exec = require('child_process').exec;
 
 var app = express();
 
@@ -38,15 +21,11 @@ app.post("/",function(req,res){
   res.end(JSON.stringify("webhook successfull"));
   console.log("webhook successfull !!");
 
-  exec('sh skberlaarhook.sh', function(error, stdout, stderr) {
+  exec("sh skberlaarhook.sh", function(error, stdout, stderr) {
                     // Log success in some manner
                     console.log( 'exec complete' );
             });
-
-  
 });
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
