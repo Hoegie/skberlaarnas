0755b97e2dcb06958c8bcbcc0de285802a77d521
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-02-19 16:11:12 +0100

    26th commit

--- skberlaarnas.js
+++ skberlaarnas.js
@@ -7,7 +7,7 @@ var join = require('path').join;
 var http = require('http');
 var path = require('path');
 /*
-version 6
+version 7
 var connection = mysql.createConnection({
   host     : '109.135.3.222',
   //host     : '192.168.69.190',
@@ -35,8 +35,8 @@ var app = express();
 
 app.get("/alarms/general",function(req,res){
 
-  res.end(JSON.stringify("test webhook finally 3e keer gelukt"));
-  console.log("test webhook finally 3e keer geslaagd !");
+  res.end(JSON.stringify("test webhook finally 7e keer gelukt"));
+  console.log("test webhook finally 7e keer geslaagd !");
 });
 
 
