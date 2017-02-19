7548acbe17e50e2d0a35fca2d0aba71ff7d024bc
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-02-19 15:22:18 +0100

    21th commit

--- skberlaarhook.sh
+++ skberlaarhook.sh
@@ -1,3 +1,3 @@
-git pull https://github.com/Hoegie/skberlaarnas.git
+/volume1/web/nodeprojects/gitHub/skberlaarnas/git pull https://github.com/Hoegie/skberlaarnas.git
 stop startnodenas
 start startnodenas
